fe36fffe32af280cd0c8cf4a8fa17c172430e747
ykaragol <ykaragol@gmail.com>
2017-08-14 00:47:22 +0300

    initial testing

---
+++ app/components/my-component.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    classNames:'reducer',
+    init(){
+        this._super(...arguments);
+        console.log('...');
+        let newStyle = document.createElement('style');
+        document.head.appendChild(newStyle);
+
+        let newStyleSheet = newStyle.sheet;
+        newStyleSheet.insertRule("@keyframes getsmaller{from {width: 100%;}to {width: 0%;}}" , newStyleSheet.cssRules.length);
+
+
+        let ms = this.get('ms');
+        console.log(`ms:${ms}`);
+        newStyleSheet.insertRule(".reducer {height: 5px;bottom: 0px;left: 0px;border-radius: inherit; width:0%;               animation-duration: "+ ms +"s;               animation-name: getsmaller;               animation-timing-function: linear;             background-color: rgb(6, 216, 1);}" , newStyleSheet.cssRules.length);
+    }
+});
